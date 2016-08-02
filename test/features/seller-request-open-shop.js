6a4536a03618f0700117b6b23da3a4b02bd7d9d7
Hoang Thanh Son <wingadium1@gmail.com>
2016-08-02 16:29:38 +0700

    [b] 423 - Seller can request to open shop when already had a pending request - Check if seller had a pending opening shop request [#127530423]

--- test/features/seller-request-open-shop.js
+++ test/features/seller-request-open-shop.js
@@ -7,6 +7,7 @@
 const helper = require('../helper');
 const request = require('supertest');
 const app = require('../../app.js');
+const ShopOpeningRequest = require('../../models').ShopOpeningRequest;
 var _ = require('lodash');
 
 describe('POST /api/v1/seller/shopOpeningRequest', () => {
@@ -85,6 +86,49 @@ describe('POST /api/v1/seller/shopOpeningRequest', () => {
             expect(res.body.note).to.equal('Em muon thanh ti phu');
           })
           .expect(200, done);
+      });
+    });
+
+    describe('with a seller who already had a pending request', () => {
+      let openingShopRequest;
+
+      before(done => {
+        ShopOpeningRequest.create({
+          name: 'An exist shop opening request',
+          description: 'with fully description',
+          address: 'C203',
+          ownerId: seller.id,
+          phone: '0123456789'
+        }).then(obj => {
+          openingShopRequest = obj;
+          done();
+        });
+      });
+
+      after(done => {
+        openingShopRequest.destroy().then(() => done());
+      });
+
+      it('should return 400 with message "A pending request is existed"', done => {
+        request(app)
+          .post('/api/v1/seller/shopOpeningRequest')
+          .set('X-Access-Token', sellerToken)
+          .set('Content-Type', 'application/json')
+          .send({
+            shopInfo: {
+              name: 'Banh My',
+              description: 'banh my XXX',
+              address: 'C203',
+              phone: '01234567890'
+            },
+            note: 'Em muon thanh ti phu'
+          })
+          .expect(res => {
+            expect(res.body.status).to.equal(400);
+            expect(res.body.message).to.equal('A pending request is existed');
+            expect(res.body.message_code).to.equal('error.open_shop_request.a_pending_request_is_existed');
+          })
+          .expect(400, done);
       });
     });
   });
